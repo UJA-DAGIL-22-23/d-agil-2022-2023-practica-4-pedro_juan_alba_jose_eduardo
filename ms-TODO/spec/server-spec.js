6ddd8a317826cac3a0b2b04a95dd535a1f5f072d
PedroTC_02 <ptc00006@red.ujaen.es>
2023-05-17 14:47:52 +0200

    Completar spec

--- ms-TODO/spec/server-spec.js
+++ ms-TODO/spec/server-spec.js
@@ -5,33 +5,20 @@ const app = require('../server');
 /**
  * Test para las rutas "estáticas": / y /acerdade
  */
-describe('Servidor PLANTILLA:', () => {
-  describe('Rutas / y /acercade', () => {
-    it('Devuelve MS Plantilla Home Page', (done) => {
-      supertest(app)
-        .get('/')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(function (res) {
-          //console.log( res.body ); // Para comprobar qué contiene exactamente res.body
-          assert(res.body.hasOwnProperty('mensaje'));
-          assert(res.body.mensaje === "Microservicio MS Plantilla: home");
+describe('Servidor Todo:', () => {
 
-        })
-        .end((error) => { error ? done.fail(error) : done() })
-    });
-    it('Devuelve MS Plantilla Acerca De', (done) => {
-      supertest(app)
-        .get('/acercade')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .expect(function (res) {
-          //console.log( "BODY ACERCA DE ", res.body ); // Para comprobar qué contiene exactamente res.body
-          assert(res.body.hasOwnProperty('mensaje'));
-          assert(res.body.mensaje === "Microservicio MS Plantilla: acerca de");
-
-        })
-        .end((error) => { error ? done.fail(error) : done() })
-    });
+describe("/getTodosNombres", () => {
+  it('Devuelve un vector de tamaño 10 al consultar mediante getTodosNombres', (done) => {
+    supertest(app)
+      .get('/getTodos')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function (res) {
+        //console.log( res.body.length ); // Para comprobar qué contiene exactamente res.body
+        assert(res.body.length === 44);
+      })
+      .end((error) => { error ? done.fail(error) : done(); }
+      );
+  });
 })
 })
