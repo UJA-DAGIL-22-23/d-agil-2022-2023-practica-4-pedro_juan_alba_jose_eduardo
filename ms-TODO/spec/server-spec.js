91299f6291c5145c51ef58889972c96b651bf9db
Juan Llavero Company <juanllavero3314@gmail.com>
2023-05-17 16:53:11 +0200

    Specs de HU-09: specs de ms-TODO implementados

--- ms-TODO/spec/server-spec.js
+++ ms-TODO/spec/server-spec.js
@@ -21,4 +21,19 @@ describe("/getTodosNombres", () => {
       );
   });
 })
+
+describe("/getTodos", () => {
+  it('Devuelve un vector de tamaño 10 al consultar mediante getTodos', (done) => {
+    supertest(app)
+      .get('/getTodosCompleto')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .expect(function (res) {
+        //console.log( res.body.length ); // Para comprobar qué contiene exactamente res.body
+        assert(res.body.length === 44);
+      })
+      .end((error) => { error ? done.fail(error) : done(); }
+      );
+  });
+})
 })
